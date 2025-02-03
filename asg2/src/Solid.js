91eb2a6757720bb7ce3b5e90d561980f97ceafee
Anurag Umale <umale.anurag111@gmail.com>
2025-02-02 21:37:07 -0800

    Removed comments and formatted the code

--- asg2/src/Solid.js
+++ asg2/src/Solid.js
@@ -1,81 +1,90 @@
 class Solid {
-    constructor(type_='', color_=[1.0, 1.0, 1.0,1.0], matrix_=new Matrix4()) {
-      if (this.constructor == Solid) {
-        throw new Error("Abstract classes can't be instantiated.");
-      }
-      this.buffer = gl.createBuffer();
-      if (!this.buffer) {
-        console.log('Failed to create the buffer object');
-        return -1;
-      }
-      this.type = type_;
-      this.color = color_;
-      this.matrix = matrix_;
-
-
-      this.reset();
+  constructor(
+    type_ = "",
+    color_ = [1.0, 1.0, 1.0, 1.0],
+    matrix_ = new Matrix4()
+  ) {
+    if (this.constructor == Solid) {
+      throw new Error("Abstract classes can't be instantiated.");
     }
-
-    reset(){
-        this.triangles = [];
-        this.tricolors = [];
+    this.buffer = gl.createBuffer();
+    if (!this.buffer) {
+      console.log("Failed to create the buffer object");
+      return -1;
     }
-  
-    setMatrix(matrix_){
-        this.matrix = matrix_;
-    }
+    this.type = type_;
+    this.color = color_;
+    this.matrix = matrix_;
 
-    translate(...a){
-        this.matrix.translate(...a);
-    }
+    this.reset();
+  }
 
-    scale(...a){
-        this.matrix.scale(...a);
-    }
+  reset() {
+    this.triangles = [];
+    this.tricolors = [];
+  }
 
-    rotate(...a){
-        this.matrix.rotate(...a);
-    }
+  setMatrix(matrix_) {
+    this.matrix = matrix_;
+  }
 
-    calculateVerts(){
-        throw new Error("Method 'calculateVerts()' must be implemented.");
-    }
+  translate(...a) {
+    this.matrix.translate(...a);
+  }
 
-    render(){
-        throw new Error("Method 'render()' must be implemented.");
-    }
+  scale(...a) {
+    this.matrix.scale(...a);
+  }
 
-    pushTriangle3D(vertices){
-        this.triangles.push(new Float32Array(vertices));
-    }
+  rotate(...a) {
+    this.matrix.rotate(...a);
+  }
 
-    render(){
-        // console.log("color length", this.tricolors.length);
-        // console.log("trian length", this.triangles.length);
+  calculateVerts() {
+    throw new Error("Method 'calculateVerts()' must be implemented.");
+  }
 
-        console.assert(this.tricolors.length == this.triangles.length, this.type,
-                        this.tricolors.length, "colors and", this.triangles.length, "triangles...")
-        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements)
-        for (let i = 0; i < this.triangles.length; i++){
-            gl.uniform4f(u_FragColor, this.tricolors[i][0], this.tricolors[i][1], this.tricolors[i][2], this.tricolors[i][3]);
-            this.drawTriangle3D(this.triangles[i]);
-        }
-    }
+  render() {
+    throw new Error("Method 'render()' must be implemented.");
+  }
 
-    drawTriangle3D(vertices) {
-        var n = vertices.length/3; // The number of vertices
-      
-        // Bind the buffer object to target
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-        // Write date into the buffer object
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-      
-        // Assign the buffer object to a_Position variable
-        gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-      
-        // Enable the assignment to a_Position variable
-        gl.enableVertexAttribArray(a_Position);
-      
-        gl.drawArrays(gl.TRIANGLES, 0, n);
+  pushTriangle3D(vertices) {
+    this.triangles.push(new Float32Array(vertices));
+  }
+
+  render() {
+    console.assert(
+      this.tricolors.length == this.triangles.length,
+      this.type,
+      this.tricolors.length,
+      "colors and",
+      this.triangles.length,
+      "triangles..."
+    );
+    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+    for (let i = 0; i < this.triangles.length; i++) {
+      gl.uniform4f(
+        u_FragColor,
+        this.tricolors[i][0],
+        this.tricolors[i][1],
+        this.tricolors[i][2],
+        this.tricolors[i][3]
+      );
+      this.drawTriangle3D(this.triangles[i]);
     }
   }
+
+  drawTriangle3D(vertices) {
+    var n = vertices.length / 3;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+
+    gl.enableVertexAttribArray(a_Position);
+
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+  }
+}
--- asg2/src/asg2.js
+++ asg2/src/asg2.js
@@ -1,26 +1,17 @@
-// ColoredPoint.js (c) 2012 matsuda
-// got some help from:
-// Drawing images on canvas: https://codepen.io/fawority/pen/aVqWey 
-// Layering canvas: https://stackoverflow.com/questions/3008635/html5-canvas-element-multiple-layers
-
-
-// Vertex shader program
 var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'uniform mat4 u_ModelMatrix;\n' +
-  'uniform mat4 u_GlobalRotateMatrix;\n' +
-  'void main() {\n' +
-  '  gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;\n' +
-  '}\n';
+  "attribute vec4 a_Position;\n" +
+  "uniform mat4 u_ModelMatrix;\n" +
+  "uniform mat4 u_GlobalRotateMatrix;\n" +
+  "void main() {\n" +
+  "  gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;\n" +
+  "}\n";
 
-// Fragment shader program
 var FSHADER_SOURCE =
-  'precision mediump float;\n' +
-  'uniform vec4 u_FragColor;\n' +  // uniform
-  'void main() {\n' +
-  '  gl_FragColor = u_FragColor;\n' +
-  '}\n';
-
+  "precision mediump float;\n" +
+  "uniform vec4 u_FragColor;\n" +
+  "void main() {\n" +
+  "  gl_FragColor = u_FragColor;\n" +
+  "}\n";
 
 let canvas;
 let gl;
@@ -32,53 +23,46 @@ let u_GlobalRotateMatrix;
 
 let jester;
 
-function setupWebGL(){
-  // Retrieve <canvas> element
-  canvas = document.getElementById('webgl');
-  
-  
-  // Get the rendering context for WebGL
-  //gl = getWebGLContext(canvas);
-  gl = canvas.getContext('webgl', {preserveDrawingBuffer: true});
+function setupWebGL() {
+  canvas = document.getElementById("webgl");
+
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
   if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
+    console.log("Failed to get the rendering context for WebGL");
     return;
   }
 }
 
-
-function connectVariablesToGLSL(){
-  // Initialize shaders
+function connectVariablesToGLSL() {
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
+    console.log("Failed to intialize shaders.");
     return;
   }
 
-  // // Get the storage location of a_Position
-  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  a_Position = gl.getAttribLocation(gl.program, "a_Position");
   if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
+    console.log("Failed to get the storage location of a_Position");
     return;
   }
 
-  // Get the storage location of u_FragColor
-  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  u_FragColor = gl.getUniformLocation(gl.program, "u_FragColor");
   if (!u_FragColor) {
-    console.log('Failed to get the storage location of u_FragColor');
+    console.log("Failed to get the storage location of u_FragColor");
     return;
   }
 
-  // Get the storage location of u_FragColor
-  u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  u_ModelMatrix = gl.getUniformLocation(gl.program, "u_ModelMatrix");
   if (!u_ModelMatrix) {
-    console.log('Failed to get the storage location of u_ModelMatrix');
+    console.log("Failed to get the storage location of u_ModelMatrix");
     return;
   }
 
-  // Get the storage location of u_FragColor
-  u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
+  u_GlobalRotateMatrix = gl.getUniformLocation(
+    gl.program,
+    "u_GlobalRotateMatrix"
+  );
   if (!u_GlobalRotateMatrix) {
-    console.log('Failed to get the storage location of u_GlobalRotateMatrix');
+    console.log("Failed to get the storage location of u_GlobalRotateMatrix");
     return;
   }
 
@@ -98,217 +82,194 @@ let g_globalYAngle = 0;
 let g_globalZAngle = 0;
 let g_zoomScale = 0.5;
 
-// 0 nothing, 1 run 2 something else if i have time
-// Crazy Meter
 let g_animation = 0;
 let g_exploding = false;
 
+let ax = ["X", "Y", "Z"];
+let appendages = ["uuparm", "foarm", "hand", "thigh", "calf", "foot"];
 
+function addActionsForHtmlUI() {
+  document
+    .getElementById("xangleSlide")
+    .addEventListener("mousemove", function (ev) {
+      if (ev.buttons == 1) {
+        g_globalXAngle = this.value;
+      }
+    });
 
-let ax = ['X', 'Y', 'Z'];
-let appendages = ["uuparm",  "foarm", "hand", "thigh",  "calf", "foot"];
-  
-function addActionsForHtmlUI(){
-  // Commented these out because no use for them anymore
+  document
+    .getElementById("yangleSlide")
+    .addEventListener("mousemove", function (ev) {
+      if (ev.buttons == 1) {
+        g_globalYAngle = this.value;
+      }
+    });
 
-  document.getElementById("xangleSlide").addEventListener('mousemove', function(ev) {
-    if (ev.buttons == 1){
-      g_globalXAngle = this.value; 
+  document
+    .getElementById("zangleSlide")
+    .addEventListener("mousemove", function (ev) {
+      if (ev.buttons == 1) {
+        g_globalZAngle = this.value;
+      }
+    });
 
-      //console.log("pp")
-    }
-  });
+  document
+    .getElementById("zoomSlide")
+    .addEventListener("mousemove", function (ev) {
+      if (ev.buttons == 1) {
+        g_zoomScale = this.value / 50;
+        renderScene();
+      }
+    });
 
-  document.getElementById("yangleSlide").addEventListener('mousemove', function(ev) {
-    if (ev.buttons == 1){
-      g_globalYAngle = this.value; 
-
-      //console.log("pp")
-    }
-  });
-
-  document.getElementById("zangleSlide").addEventListener('mousemove', function(ev) {
-    if (ev.buttons == 1){
-      g_globalZAngle = this.value; 
-
-      //console.log("pp")
-    }
-  });
-
-  document.getElementById("zoomSlide").addEventListener('mousemove', function(ev) {
-    if (ev.buttons == 1){
-      g_zoomScale = this.value/50; 
-      renderScene(); 
-      //console.log("pp")
-    }
-  });
-  
-
-  document.getElementById("runButton").onclick = function(ev){
-    console.log("hello")
+  document.getElementById("runButton").onclick = function (ev) {
+    console.log("hello");
     sendTextToHTML("Anim: Run", "anim");
     g_animation = 1;
     disableSlidersforAnim(jester.runAnim);
     jester.runStart();
-  }
+  };
 
-  document.getElementById("stopButton").onclick = function(ev){
+  document.getElementById("stopButton").onclick = function (ev) {
     sendTextToHTML("Anim: None", "anim");
     enableAllSliders();
     g_animation = 0;
-  }
+  };
 
-  document.getElementById("resetButton").onclick = function(ev){
+  document.getElementById("resetButton").onclick = function (ev) {
     sendTextToHTML("Anim: None", "anim");
     enableAllSliders();
     jester.createBody();
     jester.transformBody();
     updateAllSliders();
     g_animation = 0;
-  }
+  };
 
-  ax.forEach(x => {
-    let element = document.getElementById("head"+x+"Slide");
+  ax.forEach((x) => {
+    let element = document.getElementById("head" + x + "Slide");
     if (element != null)
-      element.addEventListener('mousemove', function(ev) {
-        if (ev.buttons == 1){
+      element.addEventListener("mousemove", function (ev) {
+        if (ev.buttons == 1) {
           jester.rotateAppendage("head", this.value, x);
         }
       });
   });
 
-  ax.forEach(x => {
-    let element = document.getElementById("pelvis"+x+"Slide");
+  ax.forEach((x) => {
+    let element = document.getElementById("pelvis" + x + "Slide");
     if (element != null)
-      element.addEventListener('mousemove', function(ev) {
-        if (ev.buttons == 1){
+      element.addEventListener("mousemove", function (ev) {
+        if (ev.buttons == 1) {
           jester.rotateAppendage("pelvis", this.value, x);
         }
       });
   });
 
-  ax.forEach(x => {
-    let element = document.getElementById("lchest"+x+"Slide");
+  ax.forEach((x) => {
+    let element = document.getElementById("lchest" + x + "Slide");
     if (element != null)
-      element.addEventListener('mousemove', function(ev) {
-        if (ev.buttons == 1){
+      element.addEventListener("mousemove", function (ev) {
+        if (ev.buttons == 1) {
           jester.rotateAppendage("lchest", this.value, x);
         }
       });
   });
 
   appendages.forEach((a) => {
-    ax.forEach(x => {
-      let lname = 'l'+a;
-      let rname = 'r'+a;
-      let element = document.getElementById(lname+x+"Slide")
+    ax.forEach((x) => {
+      let lname = "l" + a;
+      let rname = "r" + a;
+      let element = document.getElementById(lname + x + "Slide");
       if (element != null)
-        element.addEventListener('mousemove', function(ev) {
-          if (ev.buttons == 1){
+        element.addEventListener("mousemove", function (ev) {
+          if (ev.buttons == 1) {
             jester.rotateAppendage(lname, this.value, x);
-            //renderScene();
           }
         });
-      element = document.getElementById(rname+x+"Slide");
+      element = document.getElementById(rname + x + "Slide");
       if (element != null)
-        element.addEventListener('mousemove', function(ev) {
-            if (ev.buttons == 1){
-              if (x != 'X') 
-                jester.rotateAppendage(rname, -this.value, x);
-              else 
-                jester.rotateAppendage(rname, this.value, x);
-              //renderScene();
-            }
+        element.addEventListener("mousemove", function (ev) {
+          if (ev.buttons == 1) {
+            if (x != "X") jester.rotateAppendage(rname, -this.value, x);
+            else jester.rotateAppendage(rname, this.value, x);
+          }
         });
     });
-  })
+  });
 }
 
-function disableSlidersforAnim(anim){
-  for (let s in anim){
-      let name = anim[s].part
-      let x = anim[s].axis;
-      console.log(name);
-      let element = document.getElementById(name+x+"Slide");
-      if (element != null)
-        element.disabled = true;
+function disableSlidersforAnim(anim) {
+  for (let s in anim) {
+    let name = anim[s].part;
+    let x = anim[s].axis;
+    console.log(name);
+    let element = document.getElementById(name + x + "Slide");
+    if (element != null) element.disabled = true;
   }
 }
 
-function enableAllSliders(){
-  ax.forEach(x => {
-    let element = document.getElementById("head"+x+"Slide");
-    if (element != null)
-      element.disabled = false;
+function enableAllSliders() {
+  ax.forEach((x) => {
+    let element = document.getElementById("head" + x + "Slide");
+    if (element != null) element.disabled = false;
   });
 
-  ax.forEach(x => {
-    let element = document.getElementById("pelvis"+x+"Slide");
-    if (element != null)
-      element.disabled = false;
+  ax.forEach((x) => {
+    let element = document.getElementById("pelvis" + x + "Slide");
+    if (element != null) element.disabled = false;
   });
 
-  ax.forEach(x => {
-    let element = document.getElementById("lchest"+x+"Slide");
-    if (element != null)
-      element.disabled = false;
+  ax.forEach((x) => {
+    let element = document.getElementById("lchest" + x + "Slide");
+    if (element != null) element.disabled = false;
   });
 
   appendages.forEach((a) => {
-    ax.forEach(x => {
-      let lname = 'l'+a;
-      let rname = 'r'+a;
-      let element = document.getElementById(lname+x+"Slide")
-      if (element != null)
-        element.disabled = false;
-      element = document.getElementById(rname+x+"Slide");
-      if (element != null)
-        element.disabled = false;
+    ax.forEach((x) => {
+      let lname = "l" + a;
+      let rname = "r" + a;
+      let element = document.getElementById(lname + x + "Slide");
+      if (element != null) element.disabled = false;
+      element = document.getElementById(rname + x + "Slide");
+      if (element != null) element.disabled = false;
     });
-  })
+  });
 }
 
-function updateAllSliders(){
-  ax.forEach(x => {
-    let element = document.getElementById("head"+x+"Slide");
-    if (element != null)
-      element.value = jester.rotations["head"][x];
+function updateAllSliders() {
+  ax.forEach((x) => {
+    let element = document.getElementById("head" + x + "Slide");
+    if (element != null) element.value = jester.rotations["head"][x];
   });
 
-  ax.forEach(x => {
-    let element = document.getElementById("pelvis"+x+"Slide");
-    if (element != null)
-      element.value = jester.rotations["pelvis"][x]
+  ax.forEach((x) => {
+    let element = document.getElementById("pelvis" + x + "Slide");
+    if (element != null) element.value = jester.rotations["pelvis"][x];
   });
 
-  ax.forEach(x => {
-    let element = document.getElementById("lchest"+x+"Slide");
-    if (element != null)
-      element.value = jester.rotations["lchest"][x];
+  ax.forEach((x) => {
+    let element = document.getElementById("lchest" + x + "Slide");
+    if (element != null) element.value = jester.rotations["lchest"][x];
   });
 
   appendages.forEach((a) => {
-    ax.forEach(x => {
-      let lname = 'l'+a;
-      let rname = 'r'+a;
-      let element = document.getElementById(lname+x+"Slide")
-      if (element != null)
-        element.value = jester.rotations[lname][x];
-      element = document.getElementById(rname+x+"Slide");
-      if (element != null)
-        element.value = jester.rotations[rname][x];
+    ax.forEach((x) => {
+      let lname = "l" + a;
+      let rname = "r" + a;
+      let element = document.getElementById(lname + x + "Slide");
+      if (element != null) element.value = jester.rotations[lname][x];
+      element = document.getElementById(rname + x + "Slide");
+      if (element != null) element.value = jester.rotations[rname][x];
     });
-  })
+  });
 }
 
-function updateSliders(anim){
-  for (let s in anim){
+function updateSliders(anim) {
+  for (let s in anim) {
     let name = anim[s].part;
     let x = anim[s].axis;
-    let element = document.getElementById(name+x+"Slide");
-    if (element != null)
-      element.value = jester.rotations[name][x];
-
+    let element = document.getElementById(name + x + "Slide");
+    if (element != null) element.value = jester.rotations[name][x];
   }
 }
 
@@ -317,37 +278,31 @@ function main() {
 
   connectVariablesToGLSL();
 
-  addActionsForHtmlUI(); 
+  addActionsForHtmlUI();
 
-  // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
 
-  canvas.onmousemove = function(ev) { if (ev.buttons == 1) drag(ev); };
-    
-  // Specify the color for clearing <canvas>
+  canvas.onmousemove = function (ev) {
+    if (ev.buttons == 1) drag(ev);
+  };
+
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   gl.enable(gl.DEPTH_TEST);
 
-  // enable alpha blending and blendfunctions
-  // gl.enable(gl.BLEND);
-  // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-
-  // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   jester = new Jester();
   requestAnimationFrame(tick);
 }
 
-
-function convertToCoordinatesEventToGL(ev){
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
+function convertToCoordinatesEventToGL(ev) {
+  var x = ev.clientX;
+  var y = ev.clientY;
   var rect = ev.target.getBoundingClientRect();
 
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+  x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
+  y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
 
   return [x, y];
 }
@@ -355,100 +310,96 @@ function convertToCoordinatesEventToGL(ev){
 let px = 0;
 let py = 0;
 
-function click(ev){
-  if (ev.shiftKey && !g_exploding){
+function click(ev) {
+  if (ev.shiftKey && !g_exploding) {
     g_exploding = true;
     jester.explodeStart();
-  } else 
-    [px, py] = convertToCoordinatesEventToGL(ev);
-
+  } else [px, py] = convertToCoordinatesEventToGL(ev);
 }
 
 function drag(ev) {
-  if (!ev.shiftKey){
+  if (!ev.shiftKey) {
     let [x, y] = convertToCoordinatesEventToGL(ev);
-    //console.log(x, y);
-    let dy = y-py;
-    let dx = px-x;
+
+    let dy = y - py;
+    let dx = px - x;
 
     console.log(g_globalXAngle, g_globalYAngle);
-    g_globalXAngle =(g_globalXAngle + dy) %360;
-    g_globalYAngle =(g_globalYAngle +dx) % 360;
+    g_globalXAngle = (g_globalXAngle + dy) % 360;
+    g_globalYAngle = (g_globalYAngle + dx) % 360;
     document.getElementById("xangleSlide").value = g_globalXAngle;
     document.getElementById("yangleSlide").value = g_globalYAngle;
-    
   }
 }
 
+function randomizeColor(i, j) {
+  g_selectedColor[0] = Noise.perlin(
+    (g_selectedColor[0] + 0.1) / 0.9,
+    3.9 * i,
+    3.9 * j
+  );
+  g_selectedColor[1] = Noise.perlin(
+    3.9 * i,
+    (g_selectedColor[1] + 0.1) / 0.9,
+    3.9 * j
+  );
+  g_selectedColor[2] = Noise.perlin(
+    3.9 * i,
+    3.9 * j,
+    (g_selectedColor[2] + 0.1) / 0.9
+  );
 
-
-function randomizeColor(i, j){
-  g_selectedColor[0] = Noise.perlin((g_selectedColor[0]+0.1)/0.9, 3.9*i, 3.9*j);
-  g_selectedColor[1] = Noise.perlin(3.9*i, (g_selectedColor[1]+0.1)/0.9, 3.9*j);
-  g_selectedColor[2] = Noise.perlin(3.9*i, 3.9*j, (g_selectedColor[2]+0.1)/0.9);
-  
   updateSliders();
 }
 
-function renderScene(){
+function renderScene() {
   var startTime = performance.now();
- 
-  var globalRotMat = new Matrix4()
+
+  var globalRotMat = new Matrix4();
   globalRotMat.rotate(g_globalXAngle, 1, 0, 0);
   globalRotMat.rotate(g_globalYAngle, 0, 1, 0);
   globalRotMat.rotate(g_globalZAngle, 0, 0, 1);
   globalRotMat.scale(g_zoomScale, g_zoomScale, g_zoomScale);
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-
-  // const START=0.75;
-  // const RAD=1;
-  // const NUM_S = 5;
-  // for (let i = 0; i < NUM_S; i++){
-  //   let M1 = new Matrix4();
-  //   M1.translate(-1.5, lerp(i, 0, NUM_S-1, START, START-2*RAD), 0);
-  //   M1.scale(0.45, 0.45, 0.45);
-  //   drawCube([1,1,1,1], M1);
-  // }
   jester.render();
-  
+
   var duration = performance.now() - startTime;
   sendTextToHTML(
-                " ms: " + Math.floor(duration) + 
-                " fps: " + Math.floor(10000/duration), "numdot");
+    " ms: " + Math.floor(duration) + " fps: " + Math.floor(10000 / duration),
+    "numdot"
+  );
 }
 
+let g_startTime = performance.now() / 1000.0;
+let g_seconds = performance.now() / 1000.0 - g_startTime;
 
-let g_startTime=performance.now()/1000.0;
-let g_seconds=performance.now()/1000.0-g_startTime;
-
-function tick(){
-  //console.log(performance.now());
-  if (g_animation > 0)g_seconds=performance.now()/1000.0-g_startTime;
-  switch(g_animation){
-    case 1:{
-      jester.runAnimation();
-      //console.log(jester.runAnim);
-      updateSliders(jester.runAnim);
-    } break;
+function tick() {
+  if (g_animation > 0) g_seconds = performance.now() / 1000.0 - g_startTime;
+  switch (g_animation) {
+    case 1:
+      {
+        jester.runAnimation();
+        updateSliders(jester.runAnim);
+      }
+      break;
   }
-   if (g_exploding){
+  if (g_exploding) {
     jester.explodeAnimation();
-    if (!jester.isExploding){
+    if (!jester.isExploding) {
       g_exploding = false;
     }
-   }
+  }
 
   renderScene();
   requestAnimationFrame(tick);
 }
 
-function sendTextToHTML(text, htmlID){
+function sendTextToHTML(text, htmlID) {
   var htmllm = document.getElementById(htmlID);
-  if (!htmllm){
+  if (!htmllm) {
     console.log("Failed to get " + htmlID + " from HTML");
     return;
   }
